a68671febcc07975960f56fe6bf1178df8708e93
mitanshkanani <143603367+mitanshkanani@users.noreply.github.com>
2024-09-30 00:44:36 +0530

    added required dependencies

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
-
+import {Routes,Route,BrowserRouter} from 'react-router-dom'
+import Navbar from './components/Navbar';
+import Main from './pages/main/Main';
 function App() {
   return (
     <>
-
+    <BrowserRouter>
+    <Navbar />
+    <Routes>
+      <Route path='/main/*' element={<Main/>}/>
+      <Route path='/' />
+    </Routes>
+    </BrowserRouter>
     </>
   );
 }
