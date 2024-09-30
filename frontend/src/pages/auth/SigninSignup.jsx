03618e18f62ba7fd2f4f8f666c880a9a93403479
mitanshkanani <143603367+mitanshkanani@users.noreply.github.com>
2024-10-01 05:05:50 +0530

    increase the UI part of Auth

--- frontend/src/pages/auth/SigninSignup.jsx
+++ frontend/src/pages/auth/SigninSignup.jsx
@@ -99,168 +99,141 @@ function SignupSignin() {
           }}
         >
           <div className="box-login">
-            <ul>
-              <form onSubmit={handleLoginSubmit}>
-                <h1>LOGIN</h1>
-                <div className="email-login">
-                  <input
-                    className="inpt"
-                    type="email"
-                    name="email"
-                    placeholder="Email"
-                    required
-                    value={loginData.email}
-                    onChange={handleLoginInputChange}
-                  />
-                </div>
-
-                <div className="password-container">
-                  <input
-                    className="inpt"
-                    type={showPasswordLogin ? "text" : "password"}
-                    name="password"
-                    id="password-login"
-                    placeholder="Password"
-                    required
-                    value={loginData.password}
-                    onChange={handleLoginInputChange}
-                  />
-                  <i
-                    id="eye-login"
-                    className={`fa ${
-                      showPasswordLogin ? "fa-eye" : "fa-eye-slash"
-                    }`}
-                    onClick={togglePasswordVisibilityLogin}
-                    style={{ color: showPasswordLogin ? "cyan" : "white" }}
-                  ></i>
-                </div>
-                <button type="submit" className="btn">
-                  LOGIN
-                </button>
-              </form>
-              <div className="register-link">
-                <p>
-                  Don't have an account?{" "}
-                  <a id="anchor1" onClick={flip}>
-                    Register Now
-                  </a>
-                </p>
+            <form onSubmit={handleLoginSubmit}>
+              <h1>LOGIN</h1>
+              <div className="email-login">
+                <input
+                  className="inpt"
+                  type="email"
+                  name="email"
+                  placeholder="Email"
+                  required
+                  value={loginData.email}
+                  onChange={handleLoginInputChange}
+                />
               </div>
-            </ul>
+
+              <div className="password-container">
+                <input
+                  className="inpt"
+                  type={showPasswordLogin ? "text" : "password"}
+                  name="password"
+                  id="password-login"
+                  placeholder="Password"
+                  required
+                  value={loginData.password}
+                  onChange={handleLoginInputChange}
+                />
+                <i
+                  className={`fa ${
+                    showPasswordLogin ? "fa-eye" : "fa-eye-slash"
+                  }`}
+                  onClick={togglePasswordVisibilityLogin}
+                ></i>
+              </div>
+              <button type="submit" className="btn">
+                LOGIN
+              </button>
+            </form>
+            <div className="register-link">
+              <p>
+                Don't have an account?{" "}
+                <a onClick={flip}>Register Now</a>
+              </p>
+            </div>
           </div>
           <div className="box-signup">
-            <ul>
-              <form onSubmit={handleSignupSubmit}>
-                <h1>REGISTER</h1>
+            <form onSubmit={handleSignupSubmit}>
+              <h1>REGISTER</h1>
 
-                <div className="user-signup">
-                  <input
-                    className="inpt"
-                    type="text"
-                    name="firstName"
-                    id="firstName"
-                    placeholder="First Name"
-                    value={signupData.firstName}
-                    onChange={handleSignupInputChange}
-                    required
-                  />
-                  <input
-                    className="inpt"
-                    type="text"
-                    name="lastName"
-                    id="lastName"
-                    placeholder="Last Name"
-                    value={signupData.lastName}
-                    onChange={handleSignupInputChange}
-                    required
-                  />
-                </div>
-
-                <div className="username-signup">
-                  <input
-                    className="inpt"
-                    type="text"
-                    name="username"
-                    id="username-signup"
-                    placeholder="Username"
-                    value={signupData.username}
-                    onChange={handleSignupInputChange}
-                    required
-                  />
-                </div>
-
-                <div className="email-signup">
-                  <input
-                    className="inpt"
-                    type="email"
-                    name="email"
-                    id="email-signup"
-                    placeholder="Email"
-                    required
-                    value={signupData.email}
-                    onChange={handleSignupInputChange}
-                  />
-                </div>
-
-                <div className="phone-signup">
-                  <input
-                    className="inpt"
-                    type="tel"
-                    name="phone_no"
-                    id="phone-signup"
-                    placeholder="Phone"
-                    required
-                    value={signupData.phone_no}
-                    onChange={handleSignupInputChange}
-                  />
-                </div>
-
-                <div className="dob-signup">
-                  <input
-                    className="inpt"
-                    type="date"
-                    name="date_of_birth"
-                    id="dob-signup"
-                    placeholder="Date of birth"
-                    required
-                    value={signupData.date_of_birth}
-                    onChange={handleSignupInputChange}
-                  />
-                </div>
-
-                <div className="password-container">
-                  <input
-                    className="inpt"
-                    type={showPasswordSignup ? "text" : "password"}
-                    name="password"
-                    id="password-signup"
-                    placeholder="Password"
-                    required
-                    value={signupData.password}
-                    onChange={handleSignupInputChange}
-                  />
-                  <i
-                    id="eye-signup"
-                    className={`fa ${
-                      showPasswordSignup ? "fa-eye" : "fa-eye-slash"
-                    }`}
-                    onClick={togglePasswordVisibilitySignup}
-                    style={{ color: showPasswordSignup ? "cyan" : "white" }}
-                  ></i>
-                </div>
-                <button type="submit" className="btn">
-                  REGISTER
-                </button>
-              </form>
-              <div className="register-link">
-                <p>
-                  Already have an account?{" "}
-                  <a id="anchor2" onClick={flip}>
-                    Log In
-                  </a>
-                </p>
+              <div className="user-signup">
+                <input
+                  className="inpt"
+                  type="text"
+                  name="firstName"
+                  placeholder="First Name"
+                  value={signupData.firstName}
+                  onChange={handleSignupInputChange}
+                  required
+                />
+                <input
+                  className="inpt"
+                  type="text"
+                  name="lastName"
+                  placeholder="Last Name"
+                  value={signupData.lastName}
+                  onChange={handleSignupInputChange}
+                  required
+                />
               </div>
-            </ul>
+
+              <input
+                className="inpt"
+                type="text"
+                name="username"
+                placeholder="Username"
+                value={signupData.username}
+                onChange={handleSignupInputChange}
+                required
+              />
+
+              <input
+                className="inpt"
+                type="email"
+                name="email"
+                placeholder="Email"
+                required
+                value={signupData.email}
+                onChange={handleSignupInputChange}
+              />
+
+              <input
+                className="inpt"
+                type="tel"
+                name="phone_no"
+                placeholder="Phone"
+                required
+                value={signupData.phone_no}
+                onChange={handleSignupInputChange}
+              />
+
+              <input
+                className="inpt"
+                type="date"
+                name="date_of_birth"
+                placeholder="Date of birth"
+                required
+                value={signupData.date_of_birth}
+                onChange={handleSignupInputChange}
+              />
+
+              <div className="password-container">
+                <input
+                  className="inpt"
+                  type={showPasswordSignup ? "text" : "password"}
+                  name="password"
+                  placeholder="Password"
+                  required
+                  value={signupData.password}
+                  onChange={handleSignupInputChange}
+                />
+                <i
+                  className={`fa ${
+                    showPasswordSignup ? "fa-eye" : "fa-eye-slash"
+                  }`}
+                  onClick={togglePasswordVisibilitySignup}
+                ></i>
+              </div>
+              <button type="submit" className="btn">
+                REGISTER
+              </button>
+            </form>
+            <div className="register-link">
+              <p>
+                Already have an account?{" "}
+                <a onClick={flip}>Log In</a>
+              </p>
+            </div>
           </div>
         </div>
       </div>
