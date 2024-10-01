c48993a551f2a9efdd285e9f18d40359e4cdb827
mitanshkanani <143603367+mitanshkanani@users.noreply.github.com>
2024-10-01 10:53:21 +0530

    added Team section React Flow section and did needfuls in Home

--- frontend/src/pages/main/components/MainNavbar.jsx
+++ frontend/src/pages/main/components/MainNavbar.jsx
@@ -92,6 +92,14 @@ const MainNavbar = () => {
               >
                 Simulation
               </Link>
+              <Link
+                to="/main/faq"
+                className={`${
+                  isActiveLink('/main/faq') ? 'border-indigo-500 text-gray-900' : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300'
+                } inline-flex items-center px-2 pt-1 border-b-2 text-lg font-medium`}
+              >
+                FAQ
+              </Link>
             </div>
 
             {/* Auth/User Section */}
