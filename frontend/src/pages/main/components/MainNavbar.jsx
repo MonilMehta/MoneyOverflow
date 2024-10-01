c94c457afba73d1448bf1aecc90e72b191371bff
mitanshkanani <143603367+mitanshkanani@users.noreply.github.com>
2024-10-01 05:49:45 +0530

    added simulation

--- frontend/src/pages/main/components/MainNavbar.jsx
+++ frontend/src/pages/main/components/MainNavbar.jsx
@@ -84,6 +84,14 @@ const MainNavbar = () => {
               >
                 Premium
               </Link>
+              <Link
+                to="/main/simulation"
+                className={`${
+                  isActiveLink('/main/simulation') ? 'border-indigo-500 text-gray-900' : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300'
+                } inline-flex items-center px-2 pt-1 border-b-2 text-lg font-medium`}
+              >
+                Simulation
+              </Link>
             </div>
 
             {/* Auth/User Section */}
