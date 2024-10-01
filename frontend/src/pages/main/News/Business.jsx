394210d3461eb42615ba3b0e8ff6961ae7c07154
MonilMehta <monilmehta5@gmail.com>
2024-10-01 12:47:31 +0530

    Update Business.jsx

--- frontend/src/pages/main/News/Business.jsx
+++ frontend/src/pages/main/News/Business.jsx
@@ -104,7 +104,7 @@ const BusinessNewsApp = () => {
   const [country, setCountry] = useState('us'); // Initial country
 
   const fetchNews = async (page, country) => {
-    const API_KEY = '738f992c50624b6473adcfc351095f59'; // Use REACT_APP_ prefix
+    const API_KEY = process.env.REACT_APP_API_KEY;
     const API_URL = `https://gnews.io/api/v4/top-headlines?category=business&lang=en&country=${country}&apikey=${API_KEY}&page=${page}`;
     try {
       const response = await axios.get(API_URL);
