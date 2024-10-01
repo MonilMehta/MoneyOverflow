4acdf778b4651777d3122a5664f8db5b578bfee5
mitanshkanani <143603367+mitanshkanani@users.noreply.github.com>
2024-10-01 11:02:00 +0530

    completed

--- frontend/src/pages/main/components/MainNavbar.jsx
+++ frontend/src/pages/main/components/MainNavbar.jsx
@@ -58,7 +58,7 @@ const MainNavbar = () => {
                   isActiveLink('/main/blogs') ? 'border-indigo-500 text-gray-900' : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300'
                 } inline-flex items-center px-2 pt-1 border-b-2 text-lg font-medium`}
               >
-                Blogs
+                CaseStudy
               </Link>
               <Link
                 to="/main/news"
