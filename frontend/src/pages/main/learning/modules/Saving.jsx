d276bcb13ff0d584d2aa3063fb2b1ac51718bff2
Monil Mehta <monilmehta5@gmail.com>
2025-06-04 01:11:17 +0530

    Update Saving.jsx

--- frontend/src/pages/main/learning/modules/Saving.jsx
+++ frontend/src/pages/main/learning/modules/Saving.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-
+import API from "./server.api.js";
 const Savings = ({ onNextModule }) => {
   const [submodules, setSubmodules] = useState([]);
   const [loading, setLoading] = useState(true);
