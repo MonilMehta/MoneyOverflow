f08f370c83371bff401f059e849f892f86141f1c
Monil Mehta <monilmehta5@gmail.com>
2025-06-04 00:45:35 +0530

    Update Chatbot.jsx

--- frontend/src/pages/main/components/Chatbot.jsx
+++ frontend/src/pages/main/components/Chatbot.jsx
@@ -139,7 +139,7 @@ const Chatbot = () => {
 
     try {
       const API_KEY = process.env.REACT_APP_GEMINI_API_KEY;
-      const response = await fetch(`https://generativelanguage.googleapis.com/v1/models/gemini-2.0-flash:generateContent?key=${API_KEY}`, {
+      const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${API_KEY}`, {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
