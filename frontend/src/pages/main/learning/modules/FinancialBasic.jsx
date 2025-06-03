5615143b19a51b7476181f6952a3c20ec32ad398
Monil Mehta <monilmehta5@gmail.com>
2025-06-04 01:09:36 +0530

    Update FinancialBasic.jsx

--- frontend/src/pages/main/learning/modules/FinancialBasic.jsx
+++ frontend/src/pages/main/learning/modules/FinancialBasic.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import API from "./server.api.js";
 
 const FinancialBasic = ({ onNextModule }) => {
   const [submodules, setSubmodules] = useState([]);
